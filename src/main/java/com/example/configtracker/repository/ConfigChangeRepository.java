5c0706c0ba1ad04399f2ad2fcafed33efb2de5f0
simonko1811 <35770997+simonko1811@users.noreply.github.com>
2025-06-30 22:55:25 +0200

    Update ConfigChangeRepository.java

--- src/main/java/com/example/configtracker/repository/ConfigChangeRepository.java
+++ src/main/java/com/example/configtracker/repository/ConfigChangeRepository.java
@@ -2,5 +2,5 @@ package com.example.configtracker.repository;
 
 public class ConfigChangeRepository {
     //this here will be for DB integration, but it was stated that I should not use DB
-    //so this is just template for possible addition of DB integration
+    //so this is just placeholder for possible addition of DB integration
 }
